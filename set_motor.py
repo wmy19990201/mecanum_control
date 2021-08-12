9d55ef31b054c4493764fe11592e3e6e08760c74
wmy19990201 <48408239+wmy19990201@users.noreply.github.com>
2021-08-12 15:33:28 +0800

    Update set_motor.py

--- set_motor.py
+++ set_motor.py
@@ -4,22 +4,10 @@ import serial
 import time
 import numpy as np
 
-#电机转速0-4095，一个字节0-255，可将电机转速映射到字节中，字节中一个单位对应电机16个单位#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-import serial
-import time
-import numpy as np
-
 #电机转速0-4095，一个字节0-255，可将电机转速映射到字节中，字节中一个单位对应电机16个单位
 Port_ardn = 'COM3'
-#Port_ardn = '/dev/ttyUSB0'
-#---arduino = serial.Serial(Port_ardn, 9600, timeout = 1)
-
-#---arduino.flushInput()
-#---arduino.flushOutput()
 
 wheel_ctrl = [0,0,0,0]
-#send = [0,0,0,0]
 
 left_front = 00
 right_front = 00
@@ -27,35 +15,21 @@ left_rear = 00
 right_rear = 500
 end_sign = ';'
 
-def encode_spd(spd):
-    tmp = spd.to_bytes(2, byteorder='big', signed=True)#大数端存储
-    print(tmp)
-    result = tmp & bytes(0b000111111111)#语法错误
-    #print("spd:", result)
-    #return result
 
-#将int类型的wheel_ctrl数组（一共4个元素）编为12byte（起始字节+4*3）数据:0b000x xxxx xxxx
-def encode_array(wheel_ctrl):
-    #正副4095占13个二进制位，因此在2Byte中，最高3位置为0，后13位记录速度
-    def encode_spd(spd):
-        tmp = spd.to_bytes(2, byteorder='big', signed=True)#大数端存储
-        print(tmp)
-        result = tmp & 0b000111111111
-        print("spd:", result)
+def encode_array_0812(wheel_ctrl):
+    def encode_spd_0812(spd):
+        #由于chr()无法处理负数，若spd<0，则给其加10000
+        #完成映射 0-4095：0-63  -1-（-4095）：64-127
+        if (spd < 0):
+            result = int(abs(spd) / 64 + 0b1000000)
+        else:
+            result = int(spd/64)
         return result
     
-    def encode_wheelID(ID):#将轮子ID编入一个Byte中:0b111111xx
-        tmp = ID.to_bytes(1, byteorder = 'big', signed = False)
-        result = tmp | 0b11111100
-        print("ID:",result)
-        return result
+    #加入电机号、起始位（127）、终止位(126)
+    encode_array = [127, 0, encode_spd_0812(wheel_ctrl[0]), 1, encode_spd_0812(wheel_ctrl[1]), 2, encode_spd_0812(wheel_ctrl[2]), 3, encode_spd_0812(wheel_ctrl[3]), 126]
+    return encode_array         
     
-    upload_data = []
-    for i in range(0, 4):
-        upload_data.append(encode_wheelID(i))
-        upload_data.append(encode_spd(wheel_ctrl[i]))
-        
-    return upload_data
         
         
 #根据direction:0-9 和spd 设置四个电机转速wheel_ctrl[4]
@@ -122,6 +96,37 @@ def direction_ctrl(direction, spd):
     result = switch.get(direction)()
     return result
 
+class send_data:
+    def __init__(self, Port_ardn):
+        self.arduino = serial.Serial(Port_ardn, 9600, timeout = 1)
+        self.data = np.zeros(10)
+        
+    def read_data(self, data):
+        self.data = data
+    
+    def init_setup(self):
+        self.arduino.flushInput()
+        self.arduino.flushOutput()
+        
+    def send_msg(self):
+        for j in range(10):          
+            self.arduino.write(chr(self.data[j]).encode("utf-8"))
+            time.sleep(0.01)
+        print("data sent")
+        
+    def rcv_msg(self):
+        rcv = self.arduino.readline().decode()
+        print(rcv)
+        
+    def intereption(self):
+        self.arduino.write(chr(125).encode("utf-8"))
+        print("stop")
+        self.arduino.flushInput()
+        self.arduino.flushOutput()
+        self.arduino.close()
+        
+        
+
 #def direction_test():
 #    wheel_ctrl = direction_ctrl(direction, 500)
 #    direction = (direction + 1) % 11
@@ -129,72 +134,20 @@ def direction_ctrl(direction, spd):
         
 
 if __name__ == '__main__':
-    spd_encoded = encode_spd(0b101)
-    print(spd_encoded)
+    msg = encode_array_0812([0,-500,0,0])
+
+    
+    ser = send_data(Port_ardn)
+    ser.init_setup()
+    
+    try:
+        ser.init_setup()
+        ser.read_data(msg)
+        print("msg=",msg)
+        ser.send_msg()
+        ser.rcv_msg()
         
-    
-'''
-while True:
-    #right_rear = (right_rear + 16) % 4096
-    wheel_ctrl = [left_front, right_front, left_rear, right_rear]
-    
-    #print(wheel_ctrl)
-    for j in range(4):        
-        send = chr(int(wheel_ctrl[j] / 16))#发送高两位
-        #print(wheel_ctrl[j] / 16)
-        arduino.write(send.encode())
-        time.sleep(0.00)
-
-    print("wheel_spd:", wheel_ctrl)
-
+    except KeyboardInterrupt:
+        ser.intereption()
         
-    
-    #这句不能删，删了会导致周期错乱，很迷
-    #str1 = arduino.readline().decode()
-    rcv = arduino.readline().decode()
-    #print(rcv)
-'''    
-		
-    
-
-    
-
-Port_ardn = 'COM3'
-#Port_ardn = '/dev/ttyUSB0'
-arduino = serial.Serial(Port_ardn, 9600, timeout = 1)
-
-arduino.flushInput()
-arduino.flushOutput()
-
-wheel_ctrl = [0,0,0,0]
-#send = [0,0,0,0]
-
-left_front = 00
-right_front = 00
-left_rear = 00
-right_rear = 500
-end_sign = ';'
-
-while True:
-    right_rear = (right_rear + 16) % 4096
-    wheel_ctrl = [left_front, right_front, left_rear, right_rear]
-    #print(wheel_ctrl)
-    for j in range(4):        
-        send = chr(int(wheel_ctrl[j] / 16))#发送高两位
-        #print(wheel_ctrl[j] / 16)
-        arduino.write(send.encode())
-        time.sleep(0.00)
-
-    print("wheel_spd:", wheel_ctrl)
-
-        
-    
-    #这句不能删，删了会导致周期错乱，很迷
-    #str1 = arduino.readline().decode()
-    rcv = arduino.readline().decode()
-    #print(rcv)
-    
-		
-    
-
     
